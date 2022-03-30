a6f2b0273e30749ef159e730cb707c155206fe92
kcsamm <kcsamm11@gmail.com>
2022-03-30 22:33:12 +0100

    Implemented error handling

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,18 +3,33 @@ import BlogList from './BlogList';
 
 const Home = () => {
     const [ blogs, setBlogs ] = useState(null);
+    const [ isPending, setIsPending ] = useState(true);
+    const [ error, setError ] = useState(null);
 
     useEffect(() => {
-        fetch("http://localhost:8000/blogs")
-            .then( res => { return res.json() })
+        setTimeout(() => {
+            fetch("http://localhost:8000/blogs")
+            .then( res => { 
+                if( !res.ok ) throw Error('Could not fetch data');
+                return res.json() 
+            })
             .then( data => {
                 console.log( data );
-                setBlogs( data ) ;
+                setBlogs( data );
+                setIsPending( false );
+                setError( null );
+            })
+            .catch( err => {
+                setIsPending( false );
+                setError( err.message );
             });
-    }, [])
+        }, 1000);
+    }, []);
 
     return ( 
         <div className="home">
+            { error && <div>{ error }</div> }
+            { isPending && <div>Loading...</div> }
             { blogs && <BlogList blogs={ blogs } pageTitle="Blogs" /> }
             { blogs && <BlogList 
                 blogs={ blogs.filter((blog) => blog.author === 'KC Samm') } 
