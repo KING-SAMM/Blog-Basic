12e68e19b8982b8b68f5bbe500228ae565784b41
kcsamm <kcsamm11@gmail.com>
2022-04-08 23:47:40 +0100

    Implemented delete functionality

--- src/BlogDetails.jsx
+++ src/BlogDetails.jsx
@@ -1,9 +1,18 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useNavigate } from 'react-router-dom';
 import useFetch from './useFetch';
 
 const BlogDetails = () => {
     const { id } = useParams()
+    const navigate = useNavigate();
     const { data: blog, error, isPendung } = useFetch("http://localhost:8000/blogs/" + id);
+
+    const handleDelete = () => 
+    {
+        fetch('http://localhost:8000/blogs/' + blog.id, 
+        { method: 'DELETE' } )
+            .then( () =>  navigate('/') )
+            .catch( (error) =>  console.error('Error deleting blog post') );
+    }
 
     return ( 
         <div className="blog-details">
@@ -16,6 +25,7 @@ const BlogDetails = () => {
                     <article>
                         <p>{ blog.body }</p>
                     </article>
+                    <button onClick={ handleDelete }>Delete</button>
                 </section>
             ) }
         </div>
