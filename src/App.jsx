b0cc40a6ef0ba7246db02f2f55ef0b4a1d562517
kcsamm <kcsamm11@gmail.com>
2022-04-08 23:58:06 +0100

    Added 404 not found page

--- src/App.jsx
+++ src/App.jsx
@@ -7,6 +7,7 @@ import Navbar from './Navbar';
 import Create from './Create';
 import BlogDetails from './BlogDetails';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import NotFound from './NotFound';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <Route path="/" element={ <Home /> } />
             <Route path="/create" element={ <Create /> } />
             <Route path="/blogs/:id" element={ <BlogDetails /> } />
+            <Route path="*" element={ <NotFound /> } />
           </Routes>
         </div>
       </div>
